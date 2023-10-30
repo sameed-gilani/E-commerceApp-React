f6280c109ba5a32ebb1f641906d49b58f063d342
sameed-gilani <sameed.gilani@dubizzlelabs.com>
2023-10-30 11:18:44 +0500

    Setup basic initial routes

--- ecommerce_app/src/App.js
+++ ecommerce_app/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+
+import NavigationComp from "./pages/navigation";
+import HomePage from "./pages/home_page";
+import SignupPage from "./pages/signup_page";
+import LoginPage from "./pages/login_page";
+import CartPage from "./pages/cart";
 
 function App() {
+
+
+
   return (
-      <>
+      <Routes>
+
+          <Route element={<NavigationComp />}>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/SignUp' element={<SignupPage />} />
+              <Route path='/Login' element={<LoginPage />} />
+              <Route path='/Cart' element={<CartPage />} />
 
 
-      </>
+          </Route>
+
+
+      </Routes>
   );
 }
 
